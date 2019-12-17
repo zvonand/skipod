868ca44b5fbb96819f281de3ac13ebcc441cf951
zvonand <s02170105@stud.cs.msu.ru>
2019-12-17 23:03:56 +0300

    Completely rewrote MPI program
    Also added python script used for putting to queue

--- MPI/mpi_alg.cc
+++ MPI/mpi_alg.cc
@@ -2,97 +2,202 @@
 #include <cstdlib>
 #include <algorithm>
 #include <vector>
+#include <set>
 #include <iomanip>
-#include <fstream>
 #include <ctime>
 #include <cmath>
+#include <cstring>
 #include <mpi.h>
 
-#define ld long double
-#define ll unsigned long long
-#define mat ld*
-#define matRow ld*
+//#define log
 
 using namespace std;
-const double precision = 0.0001;
-
+const double precision = 0.00001;
 int size;
 
-void swapRows (mat matrix, int i, int j) {
-    for (int k = 0; k < size; ++k) {
-        swap (matrix[size*i + k], matrix[size*j + k]);
-    }
-
-}
-
-void swapColumns (mat matrix, int i, int j) {
-    for (int k = 0; k < size; ++k) {
-        swap (matrix[size*k + i], matrix[size*k + j]);
-    }
-}
-
-
-bool isNull (matRow vec) {
+bool isNull (double* matrix, int row) {
     for (int i = 0; i < size; ++i) {
-        if (abs(vec[i]) > precision) {
+        if (abs(matrix[size * row + i]) > precision) {
             return false;
         }
     }
     return true;
 }
 
-bool nullRowFound (mat matrix, int k) {
-    for (int j = k+1; j < size; ++j){
-        if (abs(matrix[size * k + j]) > precision) {
-            swapColumns(matrix, k, j);
-            return false;
+int findRank (double* matrix, int size) {
+    int ans = size;
+    for (int j = 0; j<size; ++j) {
+        if (isNull (matrix, j)) {
+            --ans;
+        } else {
+            break;
         }
     }
-    return true;
+    return ans;
 }
 
-void propagateDown (mat matrix, int k, int& nullRows) {
-    int i = size - 1 - nullRows;
-    while (isNull(&matrix[size * i])) {
-        ++nullRows;
-        --i;
-    }
-    swapRows (matrix, k, i);
-}
+double maxInColumn (double* strip, int stripHeight, int k, int& maxIndex) {
+    double max = 0;
+    maxIndex = -1;
 
-void toTrapezeMPI (mat matrix, mat trp, int rank, int nProcs, int& nullRows) {
-    MPI_Bcast (matrix, size*size, MPI_LONG_DOUBLE, 0, MPI_COMM_WORLD);
-    for (int k = 0; k < size - 1 - nullRows; ++k) {
-        /*if (nullRowFound(matrix, k)) {
-            if (!rank) {
-                propagateDown (matrix, k, nullRows);
+    for (int i = 0; i < stripHeight; ++i) {
+        if (abs(strip[i*size + k]) > abs(max)) {
+            bool suits = true;
+            for (int j = 0; j < k; ++j) {
+                if (abs (strip[i*size + j]) >= precision){
+                    suits = false;
+                    break;
+                }
             }
-            --k;
-            MPI_Bcast (matrix, size*size, MPI_LONG_DOUBLE, 0, MPI_COMM_WORLD);
-            continue;
-        }
-        MPI_Barrier (MPI_COMM_WORLD);*/
-
-        for (int i = 0; i < size*size; ++i) {
-            trp[i] = 0;
-        }
-
-        for (int j = k+1; j < size; ++j) {
-            ld tmp = matrix[size*j + k - nullRows] / matrix[size*k + k - nullRows];
-            for (int i = 0; i < size/nProcs; ++i) {
-                trp[size*j + i*size/nProcs+rank - nullRows] -= tmp * matrix[size*k + i*size/nProcs+rank - nullRows];
+            if (suits) {
+                max = strip[i*size + k];
+                maxIndex = i;
             }
         }
-        MPI_Allreduce (trp, matrix, size*size, MPI_LONG_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     }
+    return max;
+}
+
+int toTrapezeMPI (double* matrix, int rank, int nProcs) {
+    //first, divide matrix with nProcs strips, each for separate task
+
+    #ifdef log
+    cerr << "Sharing initial data started" << endl;
+    #endif
+
+    int stripHeight = (!rank) ? (size/nProcs + size%nProcs) : (size/nProcs);
+    double* strip = new double[size * stripHeight];
+    //broadcast them
+    if (!rank) {
+        memcpy(strip, matrix, stripHeight*size * sizeof(double));
+        for (int i = 1; i < nProcs; ++i) {
+            MPI_Send (&matrix[size*size/nProcs * i + size%nProcs], size*size/nProcs, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+        }
+    } else {
+        MPI_Recv (strip, stripHeight*size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        #ifdef log
+        for (int i = 0; i < stripHeight; ++i) {
+            for (int j = 0; j < size; ++j) {
+                cout << strip [i*size + j] << " ";
+            }
+            cout << endl;
+        }
+        #endif
+    }
+
+    #ifdef log
+    cerr << "Sharing initial data finished" << endl;
+    #endif
+
+    set<int> nullRows;          //to keep track of empty rows and not check them every time
+    //every process received its working strip
+
+    for (int k = 0; k < size - 1; ++k) {
+        #ifdef log
+        cerr << "Step " << k << " started" << endl;
+        #endif
+        int maxIndex;
+
+        double maxValues[nProcs] = {0};
+        double maxValues_tmp[nProcs] = {0};
+        double* candidates = new double [size * nProcs];
+        double* candidates_tmp = new double [size * nProcs];
+        fill (candidates, candidates + size*nProcs, 0);
+        fill (candidates_tmp, candidates_tmp + size*nProcs, 0);
+
+        #ifdef log
+        cerr << "Calling maxInColumn" << endl;
+        #endif
+
+        maxValues_tmp[rank] = maxInColumn (strip, stripHeight, k, maxIndex);
+
+        #ifdef log
+        cerr << "maxInColumn done, starting memcpy" << rank << endl;
+        #endif
+
+        if (maxIndex >= 0) {
+            memcpy (&candidates_tmp[size * rank], &strip[size * maxIndex], size*sizeof(double));
+        }
+
+        #ifdef log
+        cerr << "memcpy finished, allreduce started" << rank << endl;
+        #endif
+
+        //MPI_Barrier (MPI_COMM_WORLD);
+        MPI_Allreduce (candidates_tmp, candidates, size * nProcs, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        MPI_Allreduce (maxValues_tmp, maxValues, nProcs, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+        #ifdef log
+        cerr << "Allreduce finished" << rank << endl;
+        #endif
+
+        delete[] candidates_tmp;
+
+        //now every process has a copy of array of max elements and of array of candidate rows
+        //Now, choose the required index of the above mentioned arrays
+        //maxIndex == -1 means that all rows are null in the strip
+
+        if (maxIndex >= 0) {
+            int chosen = 0;
+            for (int i = 0; i < nProcs; ++i) {
+                if (abs (maxValues[chosen]) < abs (maxValues[i])) {
+                    chosen = i;
+                }
+            }
+            for (int i = 0; i < stripHeight; ++i) {
+                if (chosen == rank && maxIndex == i) {
+                    continue;
+                }
+                if (!nullRows.count (i)) {
+                    bool allNull = true;
+                    double tmp = strip[size*i + k] / candidates[chosen*size + k];
+                    for (int j = 0; j < size; ++j) {
+                        strip[size*i + j] -= tmp * candidates[chosen*size + j];
+                        if ( abs(strip[size*i + j]) > precision) {
+                            allNull = false;
+                        }
+                    }
+                    if (allNull) {
+                        nullRows.insert (i);
+                    }
+
+                }
+
+            }
+        }
+        delete[] candidates;
+
+        #ifdef log
+        cerr << "Step " << k << " finished" << endl;
+        #endif
+    }
+
+    #ifdef log
+    cerr << "elimination finished " << rank << endl;
+    #endif
+
+    int retval = 0;
+    int empties = nullRows.size();
+    MPI_Reduce (&empties, &retval, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    delete[] strip;
+    return retval;
 }
 
 
-void fillMatrix (mat matrix, int size) {
-    srand(time(NULL));
+void fillMatrix (double* matrix, int size) {
     for (int i = 0; i < size*size; ++i) {
-        matrix[i] = rand();
+        matrix[i] = rand()%1000;
     }
+    //used for testing the accuracy
+    /*
+    for (int i = 0; i < size; ++i) {
+        matrix[(size-5) * size + i] = matrix[(size-3)*size + i];
+        matrix[(size-7) * size + i] = matrix[(size-3)*size + i];
+        matrix[(size-11) * size + i] = matrix[(size-3)*size + i];
+    }*/
+
 }
 
 int main (int argc, char *argv[]) {
@@ -101,28 +206,42 @@ int main (int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &nProcs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     sscanf(argv[1], "%i", &size);
-    //To make testing easier, assume that matrix is N*N;
+    srand(time(NULL));
+
+    //To make testing easier and the testing results clearer, assume that matrix is N*N;
     //But the algorithm can be easily modified for non-square matrix
 
-    mat matrix = new ld [size * size];
-    mat trp = new ld [size * size];
+    double* matrix = new double [size * size];
     double time = 0;
-    int nullRows = 0;
-    for (int i = 0; i < 3; i++) {
+
+    for (int i = 0; i < 1; ++i) {
         if (!rank) {
             fillMatrix (matrix, size);
         }
+
         MPI_Barrier (MPI_COMM_WORLD);
         double loc_time = MPI_Wtime ();
-        toTrapezeMPI (matrix, trp, rank, nProcs, nullRows);
+
+        #ifdef log
+        cerr << "Started toTrapezeMPI" << endl;
+        #endif
+
+        int empty = toTrapezeMPI (matrix, rank, nProcs);
+
+        #ifdef log
+        cerr << "Finished toTrapezeMPI " << rank << endl;
+        #endif
+
         MPI_Barrier (MPI_COMM_WORLD);
         time += MPI_Wtime () - loc_time;
+
+        //no need to reassemble the matrix - just compute null rows in every block
         if (!rank) {
-            cout << "Rank is " << size - nullRows << endl;
+            cout << "Rank is " << size - empty << endl;
         }
     }
+
     delete[] matrix;
-    delete[] trp;
     time /= 3;
     if (!rank) {
         cout << "Time: " << fixed << setprecision(8) << time << endl;
--- MPI/run_bg.py
+++ MPI/run_bg.py
@@ -1,21 +1,18 @@
 import itertools
 import os
 
-nCpusList = [1, 2, 4, 8, 16, 32, 64, 128, 256]  # 512, 1024
-sizesList = [512, 1024, 1546, 2048, 2660, 3072]
+nCpusList = [1, 2, 4, 8, 16, 32, 64, 128, 256]  
+sizesList = [256, 512, 1024, 1546, 2048, 3072, 4096]
 
-# os.system("rm *.out *.err")
-# os.system("rm main")
 # os.system("mpixlcxx_r -o main main.cpp")
 
 for nCpus in nCpusList:
     for sz in sizesList:
-        for v in range(3):
-            fName = str(nCpus) + "_" + str(sz) + "_v" + str(v)
-            if os.path.exists(fName + ".out") or os.path.exists(fName + ".err"):
-                continue
-            os.system("mpisubmit.bg -n " + str(nCpus) +
-                      " -w 30:00"
-                      " -stdout " + fName + ".out"
-                      " -stderr " + fName + ".err"
-                      " main -- " + str(sz))
+        fName = str(nCpus) + "_" + str(sz)
+        if os.path.exists(fName + ".out") or os.path.exists(fName + ".err"):
+            continue
+        os.system("mpisubmit.bg -n " + str(nCpus) +
+                  " -w 30:00"
+                  " -stdout " + fName + ".out"
+                  " -stderr " + fName + ".err"
+                  " main -- " + str(sz))
